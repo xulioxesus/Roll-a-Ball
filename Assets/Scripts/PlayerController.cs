72961722b7e496fc7fde3ed23814175383980e40
xulioxesus <xulioxesus@gmail.com>
2021-09-10 10:57:17 +0200

    Apply force to the Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,12 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue movementValue){
         Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     void FixedUpdate(){
-        
+        Vector3 movement = new Vector3(movementX,0.0f,movementY);
+        rb.AddForce(movement);
     }
 }
