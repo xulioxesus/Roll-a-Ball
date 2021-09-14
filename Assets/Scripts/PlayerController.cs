681f6c3f112dd89a259b5330482a7726b012aac8
xulioxesus <xulioxesus@gmail.com>
2021-09-14 05:42:04 +0200

    Write a conditional statement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,6 +28,8 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other){
-        other.gameObject.SetActive(false);
+        if(other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+        }
     }
 }
